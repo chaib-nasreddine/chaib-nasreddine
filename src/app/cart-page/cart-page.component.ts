4036ccdcddccc3332f7044c9fe3adc7e2dbfc2ac
naserdine <chaibnasreddine276@gmail.com>
2022-12-06 12:27:31 +0100

    welcome

---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { cart } from '../data-type';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit {
+  cartData: cart[] | undefined;
+  constructor(private product: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+   this.loadDetails()
+
+  }
+
+  removeToCart(cartId:number|undefined){
+    cartId && this.cartData && this.product.removeToCart(cartId)
+    .subscribe((result)=>{
+      this.loadDetails();
+    })
+  }
+
+  loadDetails(){
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      console.warn(this.cartData);
+      let price = 0;
+      result.forEach((item) => {
+      })
+
+    } )
+  }
+
+}
